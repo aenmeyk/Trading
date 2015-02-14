0d83edb7c82fd0c8e0a0eb8b9bbfac602eb94330
Kevin Aenmey <kevpublic@gmail.com>
2015-02-14 09:35:42 -0500

    Add untracked files

---
+++ HistoricalPriceExtractor/Persistance/IPersister.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace HistoricalPriceExtractor.Persistance
+{
+    public interface IPersister
+    {
+        void Persist(string ticker, Stream priceStream);
+    }
+}
---
+++ HistoricalPriceExtractor/TickerProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace HistoricalPriceExtractor
+{
+    public class TickerProvider
+    {
+        public IEnumerable<string> GetTickers()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "HistoricalPriceExtractor.Tickers.txt";
+            string tickerString;
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    tickerString = reader.ReadToEnd();
+                }
+            }
+
+            var tickers = tickerString.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            return tickers;
+        }
+    }
+}
